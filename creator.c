f46fc7335b059eb959f9ab26567fbb000da17d2e
Kevin <kszpakowicz@luc.edu>
2019-12-03 22:09:09 -0600

    Add files via upload
    Added comments.

--- creator.c
+++ creator.c
@@ -3,15 +3,31 @@
 
 #include "objects.h"
 
+/**
+    Methods used to initialize game world objects, namely the Player and Environment.
+    These methods are called when starting the game to create everything needed to run.
+    All data for struct variables are read from the user-created script.
+**/
+
+
+/**
+    Creates the main character. Has health and an inventory for items (if implemented).
+**/
 create_Player(int h, char** i){
     struct player p1;
     p1.health = h;
     p1.inventory = i;
 }
 
+/**
+    Creates an evironment piece to explore. Each area consists of a name and description upon entering, as well
+    as populated objects to interact with. 
+**/
 create_Room(char* n, char* d, char** o){
     struct room &n;
     &n.name = n;
     &n.description = d;
     &n.objects = o;   
 }
+
+
