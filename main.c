657280f0f9156061edf57afacda53258bfeb8ba5
Kevin <kszpakowicz@luc.edu>
2019-12-03 22:01:38 -0600

    Add files via upload
    Added STREAM to read files

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void enter_Room(char* room){
+        
+}
+
+void start_Game(char* filename){
+    FILE *stream;
+    stream = fopen(filename, "r");    
+}
+
 int main(int argc, char *argv[]){
+    start_Game(argv[arg]);
     return 0;
 }
