88cadd0578fa96bf0afcec436ef497da64ccb3ae
Alexander Wojcik <placeboseffect@gmail.com>
2019-12-12 22:31:56 -0600

    Add files via upload

--- test.c
+++ test.c
@@ -8,5 +8,17 @@
 **/
 
 int main(int argc, char *argv[]){
-    return 0;
+    int game = 1;
+	while (game){
+		char *look = "You are in a room";
+		char *menu = "1. Look \n\n2. Action\n\n3. Move\n\n 4. Inventory\n\n"
+		char choice;
+		switch (choice){
+			case '1': printf("You see a table.");
+			case '2': printf("You cannot take the table.");
+			case '3': printf("You cannot leave.");
+			case '4': printf("Your inventory is empty.");
+		}
+		
+	}
 }
