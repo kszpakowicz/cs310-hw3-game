1d21863953053ef90ef37ed9c902fccba9a7ebe2
Alexander Wojcik <placeboseffect@gmail.com>
2019-12-12 22:30:53 -0600

    Added a test file for developing the program

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+	Text game for testing:
+	Hard-coded portions will not be in final version.
+	Parts of this file will be integrated into the final program.
+**/
+
+int main(int argc, char *argv[]){
+    return 0;
+}
