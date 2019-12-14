6d2a80ce0a508040937c1b73b1d16b20cb65f0e9
Kevin <kszpakowicz@luc.edu>
2019-12-13 23:30:10 -0600

    Add files via upload
    Added functions from creator.c. Merged into one file.

--- objects.h
+++ objects.h
@@ -23,15 +23,51 @@ typedef struct room{
     char** objects;
 } room_t;
 
-
 /**
     An object that populates a given area. Has a name and description that is read once obsereved in game.
     Additionally, has an integer value for a status. If status is equal to 0, the object cannot be picked up.
     Otherwise, it is treated as an item and can be added to the inventory.
 **/
-
 typedef struct object{
     char* name;
     char* description;
     int status;
 } object_t;
+
+/**
+    Methods used to initialize game world objects, namely the Player and Environment.
+    These methods are called when starting the game to create everything needed to run.
+    All data for struct variables are read from the user-created script.
+**/
+
+
+/**
+    Creates the main character. Has health and an inventory for items (if implemented).
+**/
+create_Player(int h, char** i){
+    struct player p1;
+    p1.health = h;
+    p1.inventory = i;
+}
+
+/**
+    Creates an evironment piece to explore. Each area consists of a name and description upon entering, as well
+    as populated objects to interact with. 
+**/
+create_Room(char* n, char* d, char** o){
+    struct room &n;
+    &n.name = n;
+    &n.description = d;
+    &n.objects = o;   
+}
+
+/**
+    Creates an object to be populated in an area. Each object has a name and description that is read once the object
+    is selected. Additionally, each object has a status that determines whether it can be picked up or not.
+**/
+create_Object(char* n, char* d, int s){
+    struct object &n;
+    &n.name = n;
+    &n.description = d;
+    &n.status = s
+}
